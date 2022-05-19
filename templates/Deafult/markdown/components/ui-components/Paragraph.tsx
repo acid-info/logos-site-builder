a8bc7f703718957d294d1f54c3dde00cd0160b35
amirhouieh <amir@status.im>
2022-05-19 15:12:03 +0200

    add paragraph component with props

--- templates/Deafult/markdown/components/ui-components/Paragraph.tsx
+++ templates/Deafult/markdown/components/ui-components/Paragraph.tsx
@@ -1,14 +1,16 @@
 import {FC, PropsWithChildren} from "react";
+import {logosCustomMarkdownLanguages} from "../../configs";
 
 interface IProps{
-
+    columns?: number
 }
 
 export const Paragraph: FC<PropsWithChildren<IProps>> = (props) => {
-    const {children} = props;
+    const {children, columns = 2} = props;
+    const cClass = new Array(columns).fill(null).map(c => `x`).join("");
     return (
-        <>
+        <article className={`${logosCustomMarkdownLanguages.paragraph} ${cClass}`}>
             {children}
-        </>
+        </article>
     )
 }
