a44a2ab4818484bde6b8c9805e0b1e18cbf4e9b8
amirhouieh <amir@status.im>
2022-06-06 15:21:44 +0200

    enable image supprt

--- templates/Academia/markdown/logos-react-md-transformers/image-uri.ts
+++ templates/Academia/markdown/logos-react-md-transformers/image-uri.ts
@@ -1,6 +1,5 @@
 import {TransformImage} from "react-markdown/lib/ast-to-react";
 
 export const transformImageUri: TransformImage = (src, alt, title) => {
-    const filename = src.split("/").pop();
-    return `/static-content/${filename}`;
+    return `/compiled-assets/${src.replace(/^\/|\/$/g, '')}`;
 }
