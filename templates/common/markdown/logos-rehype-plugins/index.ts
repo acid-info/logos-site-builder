e1f90ad45fded6d9b755c1f18b430b924980da93
amirhouieh <amir@status.im>
2022-06-07 18:16:00 +0200

    clean up

--- templates/common/markdown/logos-rehype-plugins/index.ts
+++ templates/common/markdown/logos-rehype-plugins/index.ts
@@ -2,11 +2,15 @@ import {PluggableList} from "react-markdown/lib/react-markdown";
 import rehypeSlug from 'rehype-slug'
 import rehypeLogos from "./rehype-logos-customized-blocks";
 import rehypeStringify from "rehype-stringify";
+// import rehypeMathjax from 'rehype-mathjax'
+
+
 const rehypeFigure = require("rehype-figure")
 
 export const logosRehypePlugins: PluggableList = [
     rehypeSlug,
     rehypeLogos,
     rehypeFigure,
+    // rehypeMathjax,
     rehypeStringify
 ]
