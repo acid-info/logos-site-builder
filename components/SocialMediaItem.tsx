e24209a46f99287aaf95a36a3b129934cd65e069
amirhouieh <amir@status.im>
2022-06-10 16:13:23 +0200

    close #41

--- components/SocialMediaItem.tsx
+++ components/SocialMediaItem.tsx
@@ -4,6 +4,7 @@ import {FC} from "react";
 import GithubIcon from "/public/assets/icons/github.svg";
 import DiscordIcon from "/public/assets/icons/discord.svg";
 import TwitterIcon from "/public/assets/icons/twitter.svg";
+import TelegramIcon from "/public/assets/icons/telegram.svg";
 
 
 interface IProps extends ISiteSeoSocialMedia{};
@@ -22,6 +23,9 @@ export const SocialMediaItem: FC<IProps> = (props) => (
                     case "github":
                         return <a href={`https://github.com/${props.handler}`}
                                   className={"button"}><GithubIcon/></a>
+                    case "telegram":
+                        return <a href={`https://t.me/${props.handler}`}
+                                  className={"button"}><TelegramIcon/></a>
                     default:
                         return null;
                 }
