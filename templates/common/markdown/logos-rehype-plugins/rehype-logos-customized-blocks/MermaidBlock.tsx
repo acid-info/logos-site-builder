9a221f1cb856f05972a8af3d511724499f72fee7
amirhouieh <amir@status.im>
2022-07-04 15:41:03 +0200

    addressing some issues with mermeid renders #59

--- templates/common/markdown/logos-rehype-plugins/rehype-logos-customized-blocks/MermaidBlock.tsx
+++ templates/common/markdown/logos-rehype-plugins/rehype-logos-customized-blocks/MermaidBlock.tsx
@@ -1,15 +1,26 @@
-import React, {useEffect, useRef} from "react";
+import React, {memo, useEffect, useRef} from "react";
 import mermaidAPI from 'mermaid'
 import {v4 as uuid} from 'uuid'
+import {useLogosTheme} from "../../../../../context/ThemeProvider";
+import {LogosTheme} from "../../../../../types/theme.types";
+import EThemeMode = LogosTheme.EThemeMode;
+import {useLogosSite} from "../../../../../context/SiteProvider";
 
-export const MermaidBlock = React.memo((props: { code: string }) => {
+export const MermaidBlock = memo((props: { code: string }) => {
     const ref: React.Ref<any> | null = useRef(null)
+    const {mode} = useLogosTheme();
     useEffect(() => {
+        mermaidAPI.initialize({
+            // @ts-ignore
+            theme: mode===EThemeMode.LIGHT? "base": "dark",
+        })
         if (ref.current != null) {
             mermaidAPI.render("id" + uuid().replaceAll(/[\s|-]/g, ""),
                 props.code,
-                svgCode => ref.current.innerHTML = svgCode)
+                svgCode => {
+                    ref.current.innerHTML = svgCode
+                })
         }
-    }, [])
+    }, [mode])
     return <div ref={ref}/>;
 })
